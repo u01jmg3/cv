cfb5726f760f5edd6ea718b6d56067db28b82ff1
Jonathan Goode <u01jmg3@gmail.com>
2017-02-03 15:10:49 +0000

    Use PSR-2 standard

--- make.php
+++ make.php
@@ -13,7 +13,6 @@ use Symfony\Component\Yaml\Yaml;
 use League\ColorExtractor\Palette;
 use League\ColorExtractor\Color;
 
-function createPDF($html, $filename, $stream, $dirname, $yaml){
 /**
  * Create a PDF using Dompdf with relevant options defined
  *
@@ -24,14 +23,16 @@ function createPDF($html, $filename, $stream, $dirname, $yaml){
  * @param object  $yaml
  * @return array
  */
+function createPDF($html, $filename, $stream, $dirname, $yaml)
+{
     $options = new Options();
-    $options->set(array(
+    $options->set([
         'isPhpEnabled'     => true,
         'isRemoteEnabled'  => true,
         'fontDir'          => 'fonts',
         'fontCache'        => 'fonts',
         'defaultPaperSize' => 'a4',
-    ));
+    ]);
     $dompdf = new Dompdf($options);
     // https://github.com/dompdf/dompdf/commit/23a6939
     ob_start();
@@ -40,7 +41,7 @@ function createPDF($html, $filename, $stream, $dirname, $yaml){
     $dompdf->load_html($html);
     $dompdf->render();
 
-    if($stream){
+    if ($stream) {
         $dompdf->stream($filename . '.pdf');
     } else {
         $output = $dompdf->output();
@@ -51,13 +52,6 @@ function createPDF($html, $filename, $stream, $dirname, $yaml){
 }
 
 class PDF extends FPDI_with_annots {
-    function Make($pagecount, $orientation){
-        $page_format = array(
-            'MediaBox' => array('llx' => 0,  'lly' => 0,  'urx' => 210, 'ury' => 297),
-            'CropBox'  => array('llx' => 0,  'lly' => 0,  'urx' => 210, 'ury' => 297),
-            'BleedBox' => array('llx' => 5,  'lly' => 5,  'urx' => 205, 'ury' => 292),
-            'TrimBox'  => array('llx' => 10, 'lly' => 10, 'urx' => 200, 'ury' => 287),
-            'ArtBox'   => array('llx' => 15, 'lly' => 15, 'urx' => 195, 'ury' => 282),
     /**
      * FPDI extension for TCPDF that preserves hyperlinks
      * when copying PDF pages
@@ -66,45 +60,55 @@ class PDF extends FPDI_with_annots {
      * @param string  $orientation
      * @return void
      */
+    public function Make($pagecount, $orientation)
+    {
+        $page_format = [
+            'MediaBox' => ['llx' => 0,  'lly' => 0,  'urx' => 210, 'ury' => 297],
+            'CropBox'  => ['llx' => 0,  'lly' => 0,  'urx' => 210, 'ury' => 297],
+            'BleedBox' => ['llx' => 5,  'lly' => 5,  'urx' => 205, 'ury' => 292],
+            'TrimBox'  => ['llx' => 10, 'lly' => 10, 'urx' => 200, 'ury' => 287],
+            'ArtBox'   => ['llx' => 15, 'lly' => 15, 'urx' => 195, 'ury' => 282],
             'Dur'      => 3,
-            'trans'    => array('D' => 1.5, 'S' => 'Split', 'Dm' => 'V', 'M' => 'O'),
+            'trans'    => ['D' => 1.5, 'S' => 'Split', 'Dm' => 'V', 'M' => 'O'],
             'Rotate'   => 0,
             'PZ'       => 1,
-        );
+        ];
 
-        for($loop = 1; $loop <= $pagecount; $loop++){
+        for ($loop = 1; $loop <= $pagecount; $loop++) {
             $this->AddPage($orientation, $page_format, false, false);
             $this->useTemplate($this->importPage($loop));
         }
     }
 
-    public function Header(){
     /**
      * Define a coloured header
      * and set the background colour if required
      *
      * @return void
      */
+    public function Header()
+    {
         // Check if colour is white
-        if($this->backgroundColour !== array(255, 255, 255)){
+        if ($this->backgroundColour !== [255, 255, 255]) {
             $this->Rect(0, 0, 210, 297, 'F', '', $this->backgroundColour);
         }
 
         $this->Rect(0, 0, 210, 4, 'F', '', $this->primaryColour); // 7 is max for $h (fourth parameter)
     }
 
-    public function Footer(){
     /**
      * Define a coloured footer with a centred 1D barcode of the current date
      * Include profile picture if enabled
      *
      * @return void
      */
+    public function Footer()
+    {
         $this->SetY(-13.4);
         $this->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, 0);
         $this->SetTextColor(40, 40, 40); // Darkest Grey
 
-        $style = array(
+        $style = [
             'position'     => '',
             'align'        => 'C',
             'stretch'      => false,
@@ -112,28 +116,26 @@ class PDF extends FPDI_with_annots {
             'cellfitalign' => 'C',
             'border'       => false,
             'padding'      => 0,
-            'fgcolor'      => array(40, 40, 40),
+            'fgcolor'      => [40, 40, 40],
             'bgcolor'      => false,
             'text'         => false,
-        );
+        ];
         date_default_timezone_set('Europe/London');
         // https://play.google.com/store/apps/details?id=com.google.zxing.client.android - Barcode Scanner
         $this->write1DBarcode(date('d-m-Y'), 'C128B', '', 286, 190, 3, .3, $style, 'M');
         $this->Rect(0, 293, 210, 4, 'F', '', $this->primaryColour); // 7 is max $h
 
-        if(SHOW_PROFILE_PICTURE){
+        if (SHOW_PROFILE_PICTURE) {
             $this->SetAlpha(.35);
             $image = 'images/profile.jpg';
             copy($this->image, $image); // Download locally
             $this->Circle(184, 30, 26, 0, 360, 'CNZ'); // Clipping mask
             $this->Image($image, 158, 4, 52, 52, '', '', '', true, 96);
-            $this->Circle(184, 30, 26, 0, 360, null, array('width' => 1, 'color' => array(40, 40, 40))); // Border
+            $this->Circle(184, 30, 26, 0, 360, null, ['width' => 1, 'color' => [40, 40, 40]]); // Border
         }
     }
 }
 
-function addToBookmarks($bookmarks, $array, $pagecounts){
-    if(empty($bookmarks)){
 /**
  * Create a PDF using TCPDF piped from Dompdf (using FPDI)
  * This includes creating bookmarks if a numbered title is found (e.g. `1. Title`)
@@ -145,19 +147,22 @@ function addToBookmarks($bookmarks, $array, $pagecounts){
  * @param integer $pagecounts
  * @return array
  */
+function addToBookmarks(array $bookmarks, array $array, $pagecounts)
+{
+    if (empty($bookmarks)) {
         return $array;
     } else {
-        $temp = array();
+        $temp = [];
 
-        foreach($array as $key => $value){
+        foreach ($array as $key => $value) {
             $info       = explode('|', $value);
             $pagenumber = $info[0] + $pagecounts;
             $y          = $info[1];
 
-            if(array_key_exists($key, $bookmarks)){
+            if (array_key_exists($key, $bookmarks)) {
                 $key .= " - {$pagenumber}"; // Remember array keys must be unique
             }
-            $temp[$key] = implode('|', array($pagenumber, $y));
+            $temp[$key] = implode('|', [$pagenumber, $y]);
         }
 
         $result = array_merge($bookmarks, $temp);
@@ -171,20 +176,20 @@ function addToBookmarks($bookmarks, $array, $pagecounts){
 $htmlHome        = 'html';
 $filenames       = glob("{$htmlHome}/*.php");
 $filename        = basename($filenames[0], '.php'); // First match
-$files           = array('temp/unprotected_' . rand() . '.pdf' => "P|{$filename}.php");
+$files           = ['temp/unprotected_' . rand() . '.pdf' => "P|{$filename}.php"];
 $destinationFile = "{$filename}.pdf";
 $stream          = false;
-$bookmarks       = array();
+$bookmarks       = [];
 $doBookmarks     = true;
 $pagecounts      = 0;
 $password        = 'ruc6?ray=bu6+J=neb6u';
 $yaml            = Yaml::parse(file_get_contents($htmlHome . "/{$filename}.yaml"));
 $text            = recursive_implode($yaml);
-Bookworm::configure(array('wordsPerMinute' => 275));
+Bookworm::configure(['wordsPerMinute' => 275]);
 $reading_time    = Bookworm::estimate($text, ' min');
 $yaml            = json_decode(json_encode($yaml), false);
 
-if(!$stream){
+if (!$stream) {
     // https://sourceforge.net/p/tcpdf/discussion/435311/thread/b16d9231/
     // https://stackoverflow.com/questions/5598257/tcpdf-encryption-digital-signature
     $pdf = new Pdf();
@@ -192,7 +197,7 @@ if(!$stream){
     $pdf->primaryColour    = $yaml->colours->primaryColour;
     $pdf->image            = $yaml->basics->picture;
     $pdf->SetDisplayMode($zoom = 125/*'real'*/, $layout = 'continuous', $mode = 'UseNone');
-    $preferences = array(
+    $preferences = [
         'HideMenubar'     => false,
         'HideToolbar'     => false,
         'HideWindowUI'    => false, // Scrollbars
@@ -202,20 +207,20 @@ if(!$stream){
        #'Duplex'          => 'Simplex', // Print single-sided
         'Duplex'          => 'DuplexFlipLongEdge', // Print double-sided
         'PrintScaling'    => 'None',
-    );
+    ];
     $pdf->setViewerPreferences($preferences);
     $name = $yaml->basics->name;
     $pdf->SetAuthor($name);
     $pdf->SetCreator($name);
 
-    foreach($files as $sourceFile => $config){
+    foreach ($files as $sourceFile => $config) {
         $info        = explode('|', $config);
         $orientation = $info[0];
         $htmlFile    = $htmlHome . '/' . $info[1];
 
-        if($doBookmarks){
+        if ($doBookmarks) {
             $bookmarks = addToBookmarks($bookmarks, createPDF($htmlFile, basename($sourceFile, '.pdf'), $stream, dirname($sourceFile) . '/', $yaml), $pagecounts);
-        } else if(!$doBookmarks){
+        } else if (!$doBookmarks) {
             createPDF($htmlFile, basename($sourceFile, '.pdf'), $stream, dirname($sourceFile) . '/', $yaml);
         }
         $pagecount = $pdf->setSourceFile($sourceFile);
@@ -224,14 +229,14 @@ if(!$stream){
     }
 
     // Add top of page bookmarks
-    $pages = array();
-    if($doBookmarks){
-        for($i = 1; $i <= $pagecounts; $i++){
+    $pages = [];
+    if ($doBookmarks) {
+        for ($i = 1; $i <= $pagecounts; $i++) {
             $pages[$i . '-000'] = 'Page ' . $i;
         }
     }
 
-    foreach($bookmarks as $key => $value){
+    foreach ($bookmarks as $key => $value) {
         $info       = explode('|', $value);
         $pagenumber = $info[0];
         $y          = $info[1];
@@ -240,7 +245,7 @@ if(!$stream){
 
     ksort($pages); // Sort bookmarks of Pages and Headers
 
-    foreach($pages as $key => $value){
+    foreach ($pages as $key => $value) {
         $info = explode('-', $key);
 
         $level = (strncmp($value, 'Page ', strlen('Page ')) === 0) ? 0 : 1;
@@ -248,20 +253,20 @@ if(!$stream){
         $y = isset($info[1]) ? $info[1] : 0;
         $y = (($y / 841.88999999998) * 296.64730555555);
         $y = ($y === 0) ? -1 : $y;
-        $color = array(76, 76, 76);
+        $color = [76, 76, 76];
 
         // Level 2
-        if($level === 1){
+        if ($level === 1) {
             $y += 2;
-            $color = array(88, 163, 160);
+            $color = [88, 163, 160];
         }
 
         $pdf->Bookmark($value, $level, $y, $pagenumber, $style = '', $color);
     }
 
-    $pdf->SetProtection($permissions = array('modify', 'copy', 'annot-forms', 'fill-forms', 'extract', 'assemble'), '', $password, 1, null);
+    $pdf->SetProtection($permissions = ['modify', 'copy', 'annot-forms', 'fill-forms', 'extract', 'assemble'], '', $password, 1, null);
 
-    if(extension_loaded('imagick')){
+    if (extension_loaded('imagick')) {
         $pdf_output = $pdf->Output($destinationFile, 'S');
 
         $blob = new Imagick();
@@ -279,11 +284,11 @@ if(!$stream){
 
         $html_output = '<style>body { margin: 0 }</style>';
 
-        if(file_exists($image_path)){
+        if (file_exists($image_path)) {
             $palette = Palette::fromFilename($image_path);
             $colours = $palette->getMostUsedColors(6);
 
-            $keys = array_map(function($colour){
+            $keys = array_map(function ($colour) {
                 return strtolower(ltrim(Color::fromIntToHex($colour), '#'));
             }, array_keys($colours));
             $colours = array_combine($keys, $colours);
@@ -352,9 +357,9 @@ if(!$stream){
             </style>';
 
             $html_output .= '<ul>';
-            foreach($colours as $hex){
+            foreach ($colours as $hex) {
                 // If `ffffff` trim to `fff`
-                if(preg_match('/^(.)\1*$/', $hex)){
+                if (preg_match('/^(.)\1*$/', $hex)) {
                     $hex = substr($hex, 0, 3);
                 }
 
@@ -366,7 +371,7 @@ if(!$stream){
         }
 
         $view_as_image = false;
-        if($view_as_image){
+        if ($view_as_image) {
             $html_output .= "<div class='vertically-align pdf-container'><img class='pdf-img' src='{$image_path}'></div>";
         } else {
             $pdf_encoded  = base64_encode($pdf_output);
@@ -383,7 +388,7 @@ if(!$stream){
     $unprotected = $unprotected[0];
     unlink($unprotected);
 } else {
-    foreach($files as $sourceFile => $config){
+    foreach ($files as $sourceFile => $config) {
         $info        = explode('|', $config);
         $orientation = $info[0];
         $htmlFile    = $htmlHome . '/' . $info[1];
